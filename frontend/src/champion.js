be5a00b472f7091c994ce4f4eabaffe3df46d11f
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-11-03 19:04:08 -0600

    frontend js work

---
+++ frontend/src/champion.js
@@ -0,0 +1,10 @@
+class Champion {
+    constructor(name, role, win_ratio, pick_ratio) {
+        this.name = name;
+        this.role = role;
+        this.win_ratio = win_ratio;
+        this.pick_ratio = pick_ratio;
+    }
+
+    // instance method thats going to render the object to the DOM goes here
+}
