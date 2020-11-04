d835cfb28ed0f859e230dad07363344af07d9cc1
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-11-03 19:10:46 -0600

    index work

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    debugger;
+    fetchUsers()
 })
+
+// fetch requests:
+// read - fetch users index
+
+function fetchUsers() {
+    fetch(`${BASE_URL}/users`)
+    .then(resp ==> resp)
+    .then()
+}
+// create - create a new user
+// delete - delete a user
